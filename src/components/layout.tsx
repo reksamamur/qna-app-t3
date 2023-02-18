2af91962a48da8d8b3148be1564fd3fd331f51d6
Riza Nafis <riza.nafis@gmail.com>
2023-02-18 12:12:17 +0700

    feat: create speaker page

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -1,14 +1,27 @@
+import { useSession } from "next-auth/react";
 import type { ReactElement } from "react";
 
 import Navbar from "./navbar";
 
-const Layout: React.FC<{ children: ReactElement }> = ({ children }) => {
-  return (
-    <>
-      <Navbar />
-      <main>{children}</main>
-    </>
-  );
+const Layout = ({
+  children,
+  route,
+}: {
+  children: ReactElement;
+  route?: Parameters<typeof Navbar>["0"]["route"];
+}) => {
+  const { status } = useSession();
+
+  if (status === "authenticated") {
+    return (
+      <div className="flex min-h-screen flex-col">
+        <Navbar route={route} />
+        <main className="flex w-full flex-grow bg-gray-100">{children}</main>
+      </div>
+    );
+  }
+
+  return <h3>Unauthenticated</h3>;
 };
 
 export default Layout;
--- src/pages/speaker/index.tsx
+++ src/pages/speaker/index.tsx
@@ -1,36 +1,59 @@
 import Link from "next/link";
+import { useSession } from "next-auth/react";
 
 import { api } from "../../utils/api";
 
 import Layout from "../../components/layout";
 
 import { NextPageWithLayout } from "../_app";
+import Head from "next/head";
+import { PlusIcon } from "@heroicons/react/24/outline";
 
 const SpeakerPage: NextPageWithLayout = () => {
-  const talkSessions = api.talkSession.getAllByUserId.useQuery();
+  const { status } = useSession();
+
+  const talkSessions = api.talkSession.getAllByUserId.useQuery(undefined, {
+    enabled: status === "authenticated",
+  });
 
   return (
-    <div>
-      <nav>
-        <Link href="/speaker/new-talk-session">
-          <button className="bg-primary">Create Talk Session</button>
-        </Link>
-      </nav>
-      <main>
-        {talkSessions.data?.map((talkSession) => (
-          <div key={talkSession.id}>
-            <Link href={`/talk-session/${talkSession.id}`}>
-              {talkSession.name}
+    <>
+      <Head>
+        <title>QnA App - Speaker</title>
+      </Head>
+
+      <div className="mx-auto min-h-full w-full max-w-7xl p-2 sm:p-6 lg:p-8">
+        <div className="my-4 flex justify-center md:my-2 md:justify-end">
+          <Link href="/speaker/new-talk-session">
+            <button
+              type="button"
+              className="inline-flex items-center gap-2 rounded bg-[#461091] p-2 align-middle text-sm font-semibold text-white hover:bg-[#2e026d]"
+            >
+              <PlusIcon className="h-5 w-5" aria-hidden="true" />
+              Create Talk Session
+            </button>
+          </Link>
+        </div>
+        <section className="grid gap-4">
+          <h2 className="text-xl font-semibold">Talk Session</h2>
+          {talkSessions.data?.map((talkSession) => (
+            <Link
+              key={talkSession.id}
+              href={`/talk-session/${talkSession.id}`}
+              className="grid max-w-full items-center gap-4 rounded border-2 border-indigo-800 p-4 hover:bg-indigo-800 hover:text-white"
+            >
+              <h3 className="text-xl">{talkSession.name}</h3>
+              <span>{talkSession.createdAt.toLocaleString()}</span>
             </Link>
-          </div>
-        ))}
-      </main>
-    </div>
+          ))}
+        </section>
+      </div>
+    </>
   );
 };
 
 SpeakerPage.getLayout = (page) => {
-  return <Layout>{page}</Layout>;
+  return <Layout route="Speaker">{page}</Layout>;
 };
 
 export default SpeakerPage;
